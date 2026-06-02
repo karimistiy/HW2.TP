aee68873e586af0653b21ef89e112f0de8e4a8b5
Хабутдинов Карим <kakhabutdinov@edu.hse.ru>
2026-06-02 16:49:59 +0300

    Реализован класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -72,3 +72,13 @@ class ShoppingList:
         new_list=ShoppingList()
         new_list._items=self._items+other._items
         return new_list
+class DietaryRecipe(Recipe):
+    def __init__(self,title,diet_type,ingredients=None):
+        super().__init__(title,ingredients)
+        self.diet_type=diet_type
+    def scale(self,ratio):
+        scaled=super().scale(ratio)
+        return DietaryRecipe(self.title,self.diet_type,scaled.ingredients)
+    def __str__(self):
+        return f"[{self.diet_type}] {self.title}"
+ 
