a7344c8664fdde48b888067e49863c85f3f640c4
Хабутдинов Карим <kakhabutdinov@edu.hse.ru>
2026-06-02 16:01:50 +0300

    Реализован класс ShoppingList

--- recipes.py
+++ recipes.py
@@ -47,3 +47,28 @@ class Recipe:
         for ing in self.ingredients:
             lines.append(f" - {ing}")
         return "\n".join(lines)
+class ShoppingList:
+    def __init__(self):
+        self._items=[]
+    def add_recipe(self, recipe, portions):
+        if portions<=0:
+            raise ValueError("Количество порций должно быть положительным")
+        scaled=recipe.scale(portions)
+        for ing in scaled.ingredients:
+            self._items.append((ing, recipe.title))
+    def remove_recipe(self, title):
+        self._items = [(ing, t) for (ing, t) in self._items if t != title]
+    def get_list(self):
+        totals = {}
+        for ing, i in self._items:
+            key = (ing.name,ing.unit)
+            if key in totals:
+                totals[key] += ing.quantity
+            else:
+                totals[key] = ing.quantity
+        result = [Ingredient(name,qty,unit) for (name,unit),qty in totals.items()]
+        return sorted(result,key=lambda x:x.name)
+    def __add__(self, other):
+        new_list=ShoppingList()
+        new_list._items=self._items+other._items
+        return new_list
